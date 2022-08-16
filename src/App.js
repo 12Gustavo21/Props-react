7875bc1a3ed5ec92c26048b58b5a3418dc1de7f3
12Gustavo21 <gustavo927318@gmail.com>
2022-08-16 00:56:48 -0300

    Props-project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './Header/header';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
 import GlobalStyle from './components/globalStyles';
-
 export default class App extends Component {
   render() {
     return (
